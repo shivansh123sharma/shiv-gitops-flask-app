6015d4cb7fdfa3ed8dd9d8256784e9182dd71951
shivansh sharma <shivansh1996sh@gmail.com>
2026-03-22 23:37:13 +0530

    Initialize Flask app with product catalog endpoints

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+import os
+import socket
+
+app= Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({
+        "service": "product-catalog-api",
+        "version": "1.0.0",
+        "status": "healthy"
+    })
+@app.route('/health')
+def health():
+    return jsonify({
+        "status": "healthy",
+        "hostname": socket.gethostname()
+    })
+@app.route('/products')
+def products():
+        products = [
+            {"id": 1, "name": "laptop", "price": 50000, "stock": 100},
+            {"id": 2, "name": "charger", "price": 1400, "stock": 100},
+            {"id": 3, "name": "keyboard","price": 999, "stock": 20},
+        ]
+        return jsonify({"products": products, "count": len(products)})
+if __name__== '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0'. port=port)
+  
+
+          
