163a5b2ff53e38213bf22c879b4cbc04a60c48ac
shivansh sharma <shivansh1996sh@gmail.com>
2026-03-23 12:28:23 +0530

    Fix syntax error in app.run parameters

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def products():
         return jsonify({"products": products, "count": len(products)})
 if __name__== '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0'. port=port)
+    app.run(host='0.0.0.0', port=port)
   
 
           
